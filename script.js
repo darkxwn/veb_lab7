3e503f45742b1101590d38e81cde69f7aada9990
darkxwn <zlobin_victor.off@mail.ru>
2026-04-27 15:37:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,91 @@ ymaps.ready(startApp);
 google.charts.load('current', { packages: ['corechart'] });
 
 function startApp() {
-    // Площадь Ленина
-    const origin = [48.002263, 37.805214];
-    
-    // Донбасс Арена
-    const target = [48.021074, 37.810052];
-
-    // Создание карты
     const myMap = new ymaps.Map("map", {
-        center: origin,
+        center: [48.002263, 37.805214],
         zoom: 14
     });
 
-    // Добавление круга 
-    const area = new ymaps.Circle([origin, 500], {}, {
+    let startPoint = null;
+    let endPoint = null;
+    let startPlacemark = null;
+    let endPlacemark = null;
+    let currentRoute = null;
+
+    const circleArea = new ymaps.Circle([[48.002263, 37.805214], 500], {}, {
         fillColor: "#3498db33",
         strokeColor: "#3498db",
         strokeWidth: 2
     });
-    myMap.geoObjects.add(area);
+    myMap.geoObjects.add(circleArea);
 
-    // Построение основного маршрута 
-    ymaps.route([origin, target])
-        .then(function (route) {
-            // Добавляем маршрут на карту
-            myMap.geoObjects.add(route);
-            // Расчет расстояния в километрах и времени в минутах
-            const km = (route.getLength() / 1000).toFixed(2);
-            const min = Math.round(route.getTime() / 60);
+    function updateRoute() {
+        if (startPoint && endPoint) {
+            if (currentRoute) {
+                myMap.geoObjects.remove(currentRoute);
+            }
+            ymaps.route([startPoint, endPoint])
+                .then(function (route) {
+                    currentRoute = route;
+                    myMap.geoObjects.add(route);
+                    const km = (route.getLength() / 1000).toFixed(2);
+                    const min = Math.round(route.getTime() / 60);
+                    document.getElementById("route-info").innerHTML = 
+                        `Длина пути <b>${km}</b> км • Время в дороге <b>${min}</b> мин`;
+                }).catch(function (err) {
+                    document.getElementById("route-info").innerHTML = 
+                        `<span style="color:red">Ошибка маршрута: ${err.message || err}</span>`;
+                });
+        }
+    }
 
-            // Вывод информации о маршруте пользователю
-            document.getElementById("route-info").innerHTML = 
-                `Длина пути <b>${km}</b> км • Время в дороге <b>${min}</b> мин`;
+    myMap.events.add('click', function (e) {
+        const coords = e.get('coords');
+        
+        if (!startPoint) {
+            startPoint = coords;
+            startPlacemark = new ymaps.Placemark(coords, { iconCaption: "Точка А" });
+            myMap.geoObjects.add(startPlacemark);
+        } else if (!endPoint) {
+            endPoint = coords;
+            endPlacemark = new ymaps.Placemark(coords, { iconCaption: "Точка Б" });
+            myMap.geoObjects.add(endPlacemark);
+            updateRoute();
+        } else {
+            if (currentRoute) myMap.geoObjects.remove(currentRoute);
+            startPoint = coords;
+            endPoint = null;
+            myMap.geoObjects.remove(startPlacemark);
+            myMap.geoObjects.remove(endPlacemark);
+            startPlacemark = new ymaps.Placemark(coords, { iconCaption: "Точка А" });
+            myMap.geoObjects.add(startPlacemark);
+            endPlacemark = null;
+            document.getElementById("route-info").innerHTML = "Выберите конечную точку";
+        }
+    });
 
-            // Добавление меток достопримечательностей
-            myMap.geoObjects.add(new ymaps.Placemark([48.006043, 37.802729], { iconCaption: "Театр" }));
-            myMap.geoObjects.add(new ymaps.Placemark([48.008387, 37.803934], { iconCaption: "Библиотека" }));
-        }).catch(function (err) {
-            // В случае ошибки выводим сообщение
-            document.getElementById("route-info").innerHTML = 
-                `<span style="color:red">Ошибка маршрута: ${err.message || err}</span>`;
-        });
-    // Подготовка данных для сравнительного графика
+    const origin = startPoint || [48.002263, 37.805214];
+    const target = endPoint || [48.021074, 37.810052];
+
+    if (!startPoint && !endPoint) {
+        ymaps.route([origin, target])
+            .then(function (route) {
+                myMap.geoObjects.add(route);
+                currentRoute = route;
+                const km = (route.getLength() / 1000).toFixed(2);
+                const min = Math.round(route.getTime() / 60);
+                document.getElementById("route-info").innerHTML = 
+                    `Длина пути <b>${km}</b> км • Время в дороге <b>${min}</b> мин`;
+                myMap.geoObjects.add(new ymaps.Placemark([48.006043, 37.802729], { iconCaption: "Театр" }));
+                myMap.geoObjects.add(new ymaps.Placemark([48.008387, 37.803934], { iconCaption: "Библиотека" }));
+            }).catch(function (err) {
+                document.getElementById("route-info").innerHTML = 
+                    `<span style="color:red">Ошибка маршрута: ${err.message || err}</span>`;
+            });
+    }
+    
+    document.getElementById("route-info").innerHTML = "Кликните на карте для выбора начальной точки маршрута";
+    
     prepareComparison(origin);
 }
 
